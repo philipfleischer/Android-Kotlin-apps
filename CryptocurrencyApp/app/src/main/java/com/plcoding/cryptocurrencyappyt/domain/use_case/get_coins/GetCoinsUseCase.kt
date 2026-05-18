cf1a0e5e75569439c7ba3d88191d992f45d25354
philipfleischer <fleischer1@live.no>
2026-05-18 15:59:15 +0200

    Crypto application

---
+++ CryptocurrencyApp/app/src/main/java/com/plcoding/cryptocurrencyappyt/domain/use_case/get_coins/GetCoinsUseCase.kt
@@ -0,0 +1,24 @@
+package com.plcoding.cryptocurrencyappyt.domain.use_case.get_coins
+
+import com.plcoding.cryptocurrencyappyt.common.Resource
+import com.plcoding.cryptocurrencyappyt.domain.model.Coin
+import com.plcoding.cryptocurrencyappyt.domain.repository.CoinRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+class GetCoinsUseCase @Inject constructor(
+    private val repository: CoinRepository
+) {
+    operator fun invoke(): Flow<Resource<List<Coin>>> = flow {
+        try {
+
+        } catch (e: HttpException) {
+
+        } catch (e: IOException) {
+
+        }
+    }
+}
